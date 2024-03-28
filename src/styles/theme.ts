8327088488dab90654ba055bdf04d5a4ea539e1a
Carlos Ceagah <carlosceagah@gmail.com>
2024-03-28 13:26:11 -0300

    style: create reset style, theme file and import fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    royalBlue: "#001F3F",
+    richYellow: "#FFD700",
+    darkGreen: "#228B22",
+    purple: '#800080',
+    textGray: "#333333",
+  },
+  fonts: {
+    body: "Playfair Display",
+    heading: "MedievalSharp",
+  },
+  sizes: {
+    heading: "3.5rem",
+    large: "3rem",
+    body: "1.5rem",
+    medium: "1rem",
+    small: "0.5rem",
+  },
+};
